d9b186f23ecf344d7df9931a9fb0c7b6323f0806
Nebulae <tan016666@163.com>
2025-07-30 15:18:37 +0800

    开放了接口

--- NewPlayer.js
+++ NewPlayer.js
@@ -1,5 +1,6 @@
 //LiteLoaderScript Dev Helper
 /// <reference path="c:\Users\n\Desktop\pluginstest/Library/JS/Api.js" /> 
+/// <reference path="c:\Users\n\Desktop\pluginstest/Library/JS/Api.js" />
 
 log('✅NewPlayer插件加载成功! 🧑‍💻publisher：星雲Nebulae 🏢Studio：DreamArk筑梦方舟');
 
@@ -34,3 +35,48 @@ setInterval(() => {
         old_time = time.D;
     }
 }, 360000);
+
+// 定义API函数
+function getNewPlayerCount() {
+    return jsdb.get('new_player_numbers');
+}
+
+function getTodayNewPlayerCount() {
+    return jsdb.get('EveryDay_new_player');
+}
+
+function getNewPlayerList() {
+    return jsdb.get('new_player_list');
+}
+
+function isNewPlayer(playerName) {
+    return jsdb.get('new_player_list').includes(playerName);
+}
+
+function addNewPlayer(playerName) {
+    if (!isNewPlayer(playerName)) {
+        const db = jsdb.get('new_player_list');
+        const num = jsdb.get('new_player_numbers');
+        const day = jsdb.get('EveryDay_new_player');
+
+        db.push(playerName);
+        jsdb.set('new_player_list', db);
+        jsdb.set('new_player_numbers', num + 1);
+        jsdb.set('EveryDay_new_player', day + 1);
+
+        return true;
+    }
+    return false;
+}
+
+function resetTodayCount() {
+    jsdb.set('EveryDay_new_player', 0);
+}
+
+// 暴露API
+ll.exports(getNewPlayerCount, "NewPlayer", "getNewPlayerCount");
+ll.exports(getTodayNewPlayerCount, "NewPlayer", "getTodayNewPlayerCount");
+ll.exports(getNewPlayerList, "NewPlayer", "getNewPlayerList");
+ll.exports(isNewPlayer, "NewPlayer", "isNewPlayer");
+ll.exports(addNewPlayer, "NewPlayer", "addNewPlayer");
+ll.exports(resetTodayCount, "NewPlayer", "resetTodayCount");
